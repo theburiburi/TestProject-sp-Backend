b28155b3890e5731fe69770c5bcc67a98d66f2d2
Jang99u <alswkdrb@dgu.ac.kr>
2024-04-10 14:35:55 +0900

    feat: first commit

---
+++ src/test/java/demo/dtc/DtcApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.dtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
